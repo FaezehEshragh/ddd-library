097158b72801a7a781bc55c43bf9d1cab3724867
Faezeh Eshragh <f.eshragh@gmail.com>
2020-10-26 22:55:27 -0700

    improvements to react

--- ddd-library-react/src/components/book.jsx
+++ ddd-library-react/src/components/book.jsx
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
 class book extends Component {
   state = {
-    bookName: "Factfulness",
-    author: "Hans Rosling",
-    available: "yes",
+    books: []
   };
-  render() {
-    return (
-      <div>
-        <h1>Library Books</h1>
 
-        <table>
-          <tr>
-            <th>Book Name</th>
-            <th>Author</th>
-            <th>Available</th>
-          </tr>
-          <tr>
-            <td>{this.state.bookName}</td>
-            <td>{this.state.author}</td>
-            <td>{this.state.available}</td>
-          </tr>
-        </table>
-      </div>
+  async componentDidMo11unt() {
+    const response = await fetch('/books');
+    const body = await response.json();
+    this.setState({ books: body });
+  }
+
+
+  render() {
+    const groups = this.state.books;
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <div className="App-intro">
+              <h2>Book List</h2>
+              {groups.map(group =>
+                  <div key={group.id}>
+                    {group.name}
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
     );
   }
 }
